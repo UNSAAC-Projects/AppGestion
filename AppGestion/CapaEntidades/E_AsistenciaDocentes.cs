aebdb51ec144df800d954e92676613d112f3e696
AmaruVL <182940@unsaac.edu.pe>
2021-11-18 13:15:59 -0500

    Agregación de clases en la capa de entidades

--- AppGestion/CapaEntidades/E_AsistenciaDocentes.cs
+++ AppGestion/CapaEntidades/E_AsistenciaDocentes.cs
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace CapaEntidades
 {
-    public class E_Docente: E_Tabla
+    class E_AsistenciaDocentes : E_Tabla
     {
         //==============ATRIBUTOS================
         //---   Todos heredados de cEntidad   ---
 
         //===============METODOS=================
         //-----------Constructores---------------
-        public E_Docente() : base("Docente")
+        public E_AsistenciaDocentes() : base("TAsistenciaDocentes")
         {
         }
         //--Implementacion de metodos abstractos
         public override string[] NombresAtributos()
         {
-            return new string[] {"CodDocente","APaterno", "AMaterno","Nombres","Direccion",
-                "TituloAcademico","Estado"};
+            return new string[] {"CodAsistenciaDocentes","CodDocente", "CodAsignatura",
+                "Horario","Hora","Fecha","Dia"};
         }
     }
 }
---
+++ AppGestion/CapaEntidades/E_Conexion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaEntidades
+{
+    public class cConexion
+    {   //==============ATRIBUTOS==================
+        private SqlConnection aConexion;
+        private SqlDataAdapter aAdaptador;
+        private DataSet aDatos;
+        //=============METODOS=====================
+        public cConexion()
+        {   //-- iniciar conexion
+            aDatos = new DataSet();
+            aAdaptador = new SqlDataAdapter();
+            // realizar la conexion
+            string CadenaConexion = "Data Source=.;Initial Catalog=BDTutoria;Integrated Security=True";
+            aConexion = new SqlConnection(CadenaConexion);
+        }
+        //-------------Propiedades---------------
+        public SqlConnection Conexion
+        {
+            get { return aConexion; }
+        }
+        //---------------------------------------
+        public SqlDataAdapter Adaptador
+        {
+            get { return aAdaptador; }
+        }
+        //---------------------------------------
+        public DataSet Datos
+        {
+            get { return aDatos; }
+        }
+        //-----------Servicios-------------------
+        //--- Metodos para ejecutar metodos sql server
+        //--- devuelve el resultado en la tabla cero del dataset
+        public virtual DataSet EjecutarSelect(string pConsulta)
+        {   //metodo para ejecutar consultas del tipo SELECT
+            aAdaptador.SelectCommand = new SqlCommand(pConsulta, aConexion);
+            aDatos = new DataSet();
+            aAdaptador.Fill(aDatos);
+            return aDatos;
+        }
+        //--------------------------------------------------------------------
+        //--- Metodo para ejecutasr instrucciones DML. No retorna resultados
+        //--------------------------------------------------------------------
+        public virtual void EjecutarComando(string pComando)
+        {   //metodo para ejecutar consultas del tipo INSERT, UPDATE, DELETE
+            SqlCommand oComando = new SqlCommand(pComando, aConexion);
+            aConexion.Open();
+            oComando.ExecuteNonQuery();
+            aConexion.Close();
+        }
+
+    }
+}
