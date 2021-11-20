61aa3bcff511d1cc7bd007759f7fa4940db4a2bb
lucero5796 <75134796+lucero5796@users.noreply.github.com>
2021-11-20 12:37:34 -0500

    DISENO

---
+++ AppGestion/CapaPresentacion/FormAsistenciaDocentes.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace CapaPresentacion
+{
+    partial class FormAsistenciaDocentes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Brown;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(900, 39);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(171, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(471, 69);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "                    ASISTENCIA DE DOCENTES\nDepartamento Academico de: Ingeneria I" +
+    "nofrmatica\n                ";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CapaPresentacion.Properties.Resources.Logo_unsaac11;
+            this.pictureBox2.Location = new System.Drawing.Point(729, 45);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 69);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CapaPresentacion.Properties.Resources.Logo_Sistemas1;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormAsistenciaDocentes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 553);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormAsistenciaDocentes";
+            this.Text = "FormAsistenciaDocentes";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ AppGestion/CapaPresentacion/FormListaDocentes.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace CapaPresentacion
+{
+    partial class FormListaDocentes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelLISTADOCENTE = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bunifuTileButton1 = new Bunifu.Framework.UI.BunifuTileButton();
+            this.bunifuTileButtonImprimir = new Bunifu.Framework.UI.BunifuTileButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Brown;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(827, 48);
+            this.panel1.TabIndex = 0;
+            // 
+            // labelLISTADOCENTE
+            // 
+            this.labelLISTADOCENTE.AutoSize = true;
+            this.labelLISTADOCENTE.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLISTADOCENTE.ForeColor = System.Drawing.Color.Brown;
+            this.labelLISTADOCENTE.Location = new System.Drawing.Point(95, 68);
+            this.labelLISTADOCENTE.Name = "labelLISTADOCENTE";
+            this.labelLISTADOCENTE.Size = new System.Drawing.Size(531, 69);
+            this.labelLISTADOCENTE.TabIndex = 1;
+            this.labelLISTADOCENTE.Text = "                            LISTA DE DOCENTES\n          Departamento Academico de" +
+    ": Ingeneria Inofrmatica\n                Fecha y Hora:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Brown;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(221, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Marcar todos";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Brown;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(389, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Desmarcar todos";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CapaPresentacion.Properties.Resources.Logo_unsaac11;
+            this.pictureBox2.Location = new System.Drawing.Point(688, 68);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 69);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CapaPresentacion.Properties.Resources.Logo_Sistemas1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bunifuTileButton1
+            // 
+            this.bunifuTileButton1.BackColor = System.Drawing.Color.Brown;
+            this.bunifuTileButton1.color = System.Drawing.Color.DodgerBlue;
+            this.bunifuTileButton1.colorActive = System.Drawing.Color.MediumSeaGreen;
+            this.bunifuTileButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuTileButton1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuTileButton1.ForeColor = System.Drawing.Color.White;
+            this.bunifuTileButton1.Image = global::CapaPresentacion.Properties.Resources.Guardar;
+            this.bunifuTileButton1.ImagePosition = 3;
+            this.bunifuTileButton1.ImageZoom = 20;
+            this.bunifuTileButton1.LabelPosition = 33;
+            this.bunifuTileButton1.LabelText = "GUARDAR";
+            this.bunifuTileButton1.Location = new System.Drawing.Point(440, 593);
+            this.bunifuTileButton1.Margin = new System.Windows.Forms.Padding(6);
+            this.bunifuTileButton1.Name = "bunifuTileButton1";
+            this.bunifuTileButton1.Size = new System.Drawing.Size(116, 64);
+            this.bunifuTileButton1.TabIndex = 3;
+            // 
+            // bunifuTileButtonImprimir
+            // 
+            this.bunifuTileButtonImprimir.BackColor = System.Drawing.Color.Brown;
+            this.bunifuTileButtonImprimir.color = System.Drawing.Color.DodgerBlue;
+            this.bunifuTileButtonImprimir.colorActive = System.Drawing.Color.MediumSeaGreen;
+            this.bunifuTileButtonImprimir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuTileButtonImprimir.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuTileButtonImprimir.ForeColor = System.Drawing.Color.White;
+            this.bunifuTileButtonImprimir.Image = global::CapaPresentacion.Properties.Resources.Imprimir;
+            this.bunifuTileButtonImprimir.ImagePosition = 3;
+            this.bunifuTileButtonImprimir.ImageZoom = 20;
+            this.bunifuTileButtonImprimir.LabelPosition = 33;
+            this.bunifuTileButtonImprimir.LabelText = "IMPRIMIR";
+            this.bunifuTileButtonImprimir.Location = new System.Drawing.Point(221, 594);
+            this.bunifuTileButtonImprimir.Margin = new System.Windows.Forms.Padding(6);
+            this.bunifuTileButtonImprimir.Name = "bunifuTileButtonImprimir";
+            this.bunifuTileButtonImprimir.Size = new System.Drawing.Size(116, 63);
+            this.bunifuTileButtonImprimir.TabIndex = 2;
+            // 
+            // FormListaDocentes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 684);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.bunifuTileButton1);
+            this.Controls.Add(this.bunifuTileButtonImprimir);
+            this.Controls.Add(this.labelLISTADOCENTE);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormListaDocentes";
+            this.Text = "FormListaDocentes";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelLISTADOCENTE;
+        private Bunifu.Framework.UI.BunifuTileButton bunifuTileButtonImprimir;
+        private Bunifu.Framework.UI.BunifuTileButton bunifuTileButton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
