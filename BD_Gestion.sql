b5abe64cac404c480b67b4d05d2ef2af1238a6d8
luis_jh <160544@unsaac.edu.pe>
2021-11-23 00:48:00 -0500

    Update BD_Gestion.sql

--- BD_Gestion.sql
+++ BD_Gestion.sql
@@ -80,7 +80,7 @@ CREATE TABLE THorario
 	HoraInicio varchar(2),
 	HoraFin varchar(2),
 	IDCatalogo varchar(6),
-	Tipo varchar(10), --(teorico o pr·ctico)
+	Tipo varchar(10), --(teorico o pr√°ctico)
 	PRIMARY KEY (IDHorario),
 	FOREIGN KEY (IDCatalogo) REFERENCES TCatalogo
 )
@@ -169,7 +169,6 @@ CREATE TABLE TMatriculado
 GO
 
 ------------------------------- PROCEDIMIENTO ALMACENADOS DE ASIGNATURA
-
 create proc SP_LISTARASIGNATURA
 as
 select CodAsignatura, IDPlan,Nombre, Creditos, Categoria, HorasTeoricas, HorasPracticas, Prerrequisitos from TAsignatura
@@ -217,6 +216,55 @@ delete TAsignatura
 where CodAsignatura=@CodAsignatura
 go
 
+------------------------------ PROCEDIMIENTO ALMACENADOS DE CURSO CATALOGO
+
+create proc SP_LISTARCATALOGO
+as
+select NroSemestre, CodAsignatura, Grupo, Aula ,CodDocenteTeorico, CodDocentePractico from TCatalogo
+go
+
+
+CREATE PROC SP_BUSCARCATALOGO
+@BUSCAR varchar(20)
+as
+select * from TCatalogo
+where NroSemestre like @BUSCAR + '%' or  CodAsignatura like @BUSCAR + '%' 
+go
+
+
+create proc SP_INSERTARCATALOGO
+	@IDCatalogo varchar(6),
+	@NroSemestre varchar(2),
+	@CodAsignatura varchar(6),
+	@Grupo varchar(1),
+	@Aula varchar(6),
+	@CodDocenteTeorico varchar(6),
+	@CodDocentePractico varchar(6)
+as
+insert into TCatalogo values(@IDCatalogo,@NroSemestre,@CodAsignatura,@Grupo,@Aula,@CodDocentePractico,@CodDocenteTeorico)
+go
+
+create proc SP_EDITARCATALOGO
+	@IDCatalogo varchar(6),
+	@NroSemestre varchar(2),
+	@CodAsignatura varchar(6),
+	@Grupo varchar(1),
+	@Aula varchar(6),
+	@CodDocenteTeorico varchar(6),
+	@CodDocentePractico varchar(6)
+as 
+update TCatalogo set NroSemestre=@NroSemestre, CodAsignatura=@CodAsignatura,Grupo=@Grupo,Aula=@Aula,CodDocentePractico=@CodDocentePractico, CodDocenteTeorico=@CodDocenteTeorico
+where IDCatalogo =@IDCatalogo
+go
+
+create proc SP_ELIMINARCATALOGO
+@IDCatalogo varchar(10)
+as
+delete from THorario where IDCatalogo=@IDCatalogo
+delete from TCatalogo where IDCatalogo=@IDCatalogo
+go
+
+
 
 ----------------------  PROC.  HORARIO -----------------
 CREATE PROC SP_INSERTARHORARIO
@@ -229,17 +277,11 @@ AS INSERT INTO THorario values (
 	@Dia,
 	@HoraInicio,
 	@HoraFin,
-	@Tipo,
-	@IDCatalogo )
+	@IDCatalogo,
+	@Tipo )
 go
+drop proc SP_INSERTARHORARIO
 
-------- LISTAR HORARIO -------------
-
-create proc SP_VISTAHORARIOS
-as
-select C.CodAsignatura, C.Grupo, h.Dia ,h.HoraInicio, h.HoraFin, h.Tipo
-from TCatalogo C inner join THorario H on C.IDCatalogo=H.IDCatalogo
-go
 
 ----------------------  PROC. VISTA CATALOGO ------------------------------------------------------
 CREATE PROC SP_VISTACATALOGO
@@ -257,9 +299,14 @@ from TCatalogo C inner join TAsignatura A on C.CodAsignatura=A.CodAsignatura
 where A.Nombre like @BUSCAR + '%'
 go
 
+
 ------- LISTAR HORARIO -------------
+
 create proc SP_VISTAHORARIOS
 as
-select C.CodAsignatura, C.Grupo, h.Dia ,h.HoraInicio, h.HoraFin
+select C.CodAsignatura, C.Grupo, h.Dia ,h.HoraInicio, h.HoraFin, h.Tipo
 from TCatalogo C inner join THorario H on C.IDCatalogo=H.IDCatalogo
 go
+
+INSERT INTO TDocente values ('D000','NO DEFINIDO' ,'','','')
+GO
