7d6ea3a536aadf424edb35f59008833d811cbd17
LuzLMC <83050017+lmc1221@users.noreply.github.com>
2021-11-22 23:22:27 -0500

    BD Actualizada

---
+++ BD_Gestion.sql
@@ -0,0 +1,259 @@
+GO
+use master
+go
+DROP DATABASE AppGestion
+GO
+create database AppGestion
+go
+
+use AppGestion
+go
+
+
+/***************************************************************
+						CREACION TABLAS
+****************************************************************/
+
+CREATE TABLE TPlanDeEstudios
+(
+	IDPlan varchar(6),
+	CodDepartamento varchar(6),
+	PRIMARY KEY (IDPlan) 
+)
+GO
+
+CREATE TABLE TAsignatura
+(
+	CodAsignatura varchar(6),
+	IDPlan varchar(6),
+	Nombre varchar(100),
+	Creditos varchar(2),
+	Categoria varchar(100),
+	HorasPracticas varchar(2),
+	HorasTeoricas varchar(2),
+	Prerrequisitos varchar(100),
+	PRIMARY KEY (CodAsignatura),
+	FOREIGN KEY (IDPlan) REFERENCES TPlanDeEstudios
+)
+GO
+
+CREATE TABLE TAlumno
+(
+	CodAlumno varchar(6),
+	Nombres varchar(100),
+	Apellidos varchar(100),
+	PRIMARY KEY (CodAlumno) 
+)
+GO
+
+CREATE TABLE TDocente
+(
+	CodDocente varchar(6),
+	Nombres varchar(100),
+	Apellidos varchar(100),
+	TituloAcademico varchar(100),
+	Estado varchar (100),
+	PRIMARY KEY (CodDocente) 
+)
+GO
+
+CREATE TABLE TCatalogo
+(
+	IDCatalogo varchar(6),
+	NroSemestre varchar(2),
+	CodAsignatura varchar(6),
+	Grupo varchar(1),
+	Aula varchar(6),
+	CodDocentePractico varchar(6),
+	CodDocenteTeorico varchar(6),
+	PRIMARY KEY (IDCatalogo),
+	FOREIGN KEY (CodAsignatura) REFERENCES TAsignatura,
+	FOREIGN KEY (CodDocentePractico) REFERENCES TDocente,
+	FOREIGN KEY (CodDocenteTeorico) REFERENCES TDocente
+)
+GO
+
+CREATE TABLE THorario
+(
+	IDHorario INT IDENTITY,
+	Dia varchar(100),
+	HoraInicio varchar(2),
+	HoraFin varchar(2),
+	IDCatalogo varchar(6),
+	Tipo varchar(10), --(teorico o práctico)
+	PRIMARY KEY (IDHorario),
+	FOREIGN KEY (IDCatalogo) REFERENCES TCatalogo
+)
+GO
+
+CREATE TABLE TAsistencia
+(
+	IDAsistencia varchar(6),
+	Fecha varchar(10),
+	Hora varchar(2),
+	Tipo varchar(100),
+	IDHorario INT IDENTITY,
+	PRIMARY KEY (IDAsistencia),
+	FOREIGN KEY (IDHorario) REFERENCES THorario
+)
+GO
+
+CREATE TABLE TAsistenciaAlumno
+(
+	IDAsistenciaAlumno varchar(6),
+	IDAsistencia varchar(6),
+	Asistio varchar(60),
+	Observaciones varchar(100),
+	PRIMARY KEY (IDAsistenciaAlumno),
+	FOREIGN KEY (IDAsistencia) REFERENCES TAsistencia
+)
+GO
+
+CREATE TABLE TAsistenciaDiariaDocente
+(
+	IDAsistenciaDiaria varchar(6),
+	Fecha varchar(10),
+	Hora varchar(2),
+	PRIMARY KEY (IDAsistenciaDiaria) 
+)
+GO
+
+CREATE TABLE TAsistenciaDocente
+(
+	IDAsistenciaDocente varchar(6),
+	IDAsistencia varchar(6),
+	Semestre varchar(2),
+	TemaAvance varchar(100),
+	Asistio varchar(60),
+	PRIMARY KEY (IDAsistenciaDocente),
+	FOREIGN KEY (IDAsistencia) REFERENCES TAsistencia
+)
+GO
+
+CREATE TABLE TListadoDocentes
+(
+	IDListado varchar(6),
+	IDAsistenciaDiaria varchar(6),
+	Asistio varchar(60),
+	CodDocente varchar(6),
+	Observaciones varchar(100),
+	PRIMARY KEY (IDListado),
+	FOREIGN KEY (IDAsistenciaDiaria) REFERENCES TAsistenciaDiariaDocente,
+	FOREIGN KEY (CodDocente) REFERENCES TDocente
+
+)
+GO
+
+CREATE TABLE TLogin
+(
+	Usuario varchar(60) NOT NULL, 
+	Contrasenia varchar(60),
+	Categoria varchar(100) NOT NULL,
+	CodDocente varchar(6),
+	PRIMARY KEY (Usuario, Categoria),
+	FOREIGN KEY (CodDocente) REFERENCES TDocente
+)
+GO
+
+CREATE TABLE TMatriculado
+(
+	IDMatriculado varchar(6),
+	CodAlumno varchar(6),
+	Semestre varchar(2),
+	Grupo varchar(1),
+	IDCatalogo varchar(6),
+	PRIMARY KEY (IDMatriculado),
+	FOREIGN KEY (CodAlumno) REFERENCES TAlumno,
+	FOREIGN KEY (IDCatalogo) REFERENCES TCatalogo
+)
+GO
+
+------------------------------- PROCEDIMIENTO ALMACENADOS DE ASIGNATURA
+
+create proc SP_LISTARASIGNATURA
+as
+select CodAsignatura, IDPlan,Nombre, Creditos, Categoria, HorasTeoricas, HorasPracticas, Prerrequisitos from TAsignatura
+go
+
+CREATE PROC SP_BUSCARASIGNATURA
+@BUSCAR varchar(20)
+as
+select * from TAsignatura
+where CodAsignatura like @BUSCAR + '%' or  Nombre like @BUSCAR + '%'
+go
+
+create proc SP_INSERTARASIGNATURA
+	@CodAsignatura varchar(6),
+	@IDPlan varchar(6),
+	@Nombre varchar(100),
+	@Creditos varchar(2),
+	@Categoria varchar(100),
+	@HorasPracticas varchar(2),
+	@HorasTeoricas varchar(2),
+	@Prerrequisitos varchar(100)
+as
+insert into TAsignatura values(@CodAsignatura,@IDPlan,@Nombre,@Creditos,@Categoria,@HorasPracticas,@HorasTeoricas,@Prerrequisitos)
+go
+
+create proc SP_EDITARASIGNATURA
+@CodAsignatura varchar(6),
+	@IDPlan varchar(6),
+	@Nombre varchar(100),
+	@Creditos varchar(2),
+	@Categoria varchar(100),
+	@HorasPracticas varchar(2),
+	@HorasTeoricas varchar(2),
+	@Prerrequisitos varchar(100)
+as 
+update TAsignatura set IDPlan=@IDPlan, Nombre=@Nombre,Creditos=@Creditos,Categoria=@Categoria,HorasPracticas=@HorasPracticas,
+		@HorasTeoricas=@HorasTeoricas,Prerrequisitos=@Prerrequisitos
+where CodAsignatura =@CodAsignatura
+go
+
+create proc SP_ELIMINARASIGNATURA
+@CodAsignatura varchar(10)
+as
+delete TAsignatura
+where CodAsignatura=@CodAsignatura
+go
+
+
+----------------------  PROC.  HORARIO -----------------
+CREATE PROC SP_INSERTARHORARIO
+	@Dia varchar(100),
+	@HoraInicio varchar(2),
+	@HoraFin varchar(2),
+	@Tipo varchar(10),
+	@IDCatalogo varchar(6)
+AS INSERT INTO THorario values (
+	@Dia,
+	@HoraInicio,
+	@HoraFin,
+	@Tipo,
+	@IDCatalogo )
+go
+
+------- LISTAR HORARIO -------------
+
+create proc SP_VISTAHORARIOS
+as
+select C.CodAsignatura, C.Grupo, h.Dia ,h.HoraInicio, h.HoraFin, h.Tipo
+from TCatalogo C inner join THorario H on C.IDCatalogo=H.IDCatalogo
+go
+
+----------------------  PROC. VISTA CATALOGO ------------------------------------------------------
+CREATE PROC SP_VISTACATALOGO
+--@BUSCAR varchar(20)
+as
+select C.IDCatalogo,C.CodAsignatura ,C.CodAsignatura + C.Grupo +'IN' as GrupoAsignatura,A.Nombre, A.Creditos , A.Categoria, C.NroSemestre
+from TCatalogo C inner join TAsignatura A on C.CodAsignatura=A.CodAsignatura 
+go
+
+CREATE PROC SP_BUSCARVISTACATALOGO
+@BUSCAR varchar(20)
+as
+select C.IDCatalogo, C.CodAsignatura,C.CodAsignatura + C.Grupo +'IN' as GrupoAsignatura,A.Nombre,  A.Creditos , A.Categoria, C.NroSemestre
+from TCatalogo C inner join TAsignatura A on C.CodAsignatura=A.CodAsignatura
+where A.Nombre like @BUSCAR + '%'
+go
+
