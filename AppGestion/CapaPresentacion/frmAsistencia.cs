de50e285ad20ac60f4b6fdc1fa1fb841195d0c6e
katheryne <OEM@LAPTOP-IUT020T4>
2021-12-20 22:39:49 -0500

    Actualizar

--- AppGestion/CapaPresentacion/frmAsistencia.cs
+++ AppGestion/CapaPresentacion/frmAsistencia.cs
@@ -76,28 +76,7 @@ namespace CapaPresentacion
         }
         public void ExportarDatos(DataGridView datalistado)
         {
-            //Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application(); // Instancia a la libreria de Microsoft Office
-            //excel.Application.Workbooks.Add(true); //Con esto añadimos una hoja en el Excel para exportar los archivos
-            //int IndiceColumna = 0;
-            //foreach (DataGridViewColumn columna in datalistado.Columns) //Aquí empezamos a leer las columnas del listado a exportar
-            //{
-            //    IndiceColumna++;
-            //    excel.Cells[1, IndiceColumna] = columna.Name;
-            //}
-            //int IndiceFila = 0;
-            //foreach (DataGridViewRow fila in datalistado.Rows) //Aquí leemos las filas de las columnas leídas
-            //{
-            //    IndiceFila++;
-            //    IndiceColumna = 0;
-            //    foreach (DataGridViewColumn columna in datalistado.Columns)
-            //    {
-            //        IndiceColumna++;
-            //        excel.Cells[IndiceFila + 1, IndiceColumna] = fila.Cells[columna.Name].Value;
-            //    }
-            //}
-            //excel.Visible = false;
-
-            //MessageBox.Show(excel.ToString());
+            
             string name = datos.NombreCurso;
 
             string ruta = @"D:\8vosemestre\Ing.Software\proyecto\ListaAlumnosDia\"+name+".xlsx";
