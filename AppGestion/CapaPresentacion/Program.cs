f446fe3ff3522b5934cb602327f400c7c6743ad8
AmaruVL <182940@unsaac.edu.pe>
2022-01-15 20:54:50 -0500

    cambios

--- AppGestion/CapaPresentacion/Program.cs
+++ AppGestion/CapaPresentacion/Program.cs
@@ -20,7 +20,7 @@ namespace CapaPresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            AppContext = new ApplicationContext(new frmAsistenciaDiariaDocentes());
+            AppContext = new ApplicationContext(new FrmLogin());
             Application.Run(AppContext);
         }
 
