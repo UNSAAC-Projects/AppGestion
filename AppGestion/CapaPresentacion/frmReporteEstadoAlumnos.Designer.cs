aa99f58809da6eee3456bb21ee8edfa357b6c5ff
AmaruVL <182940@unsaac.edu.pe>
2022-01-31 02:04:07 -0500

    Correccion frmReporteEstadoDocentes

--- AppGestion/CapaPresentacion/frmReporteEstadoAlumnos.Designer.cs
+++ AppGestion/CapaPresentacion/frmReporteEstadoAlumnos.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CapaPresentacion
 {
-    partial class frmReporteEstadoDocentes
+    partial class frmReporteEstadoAlumnos
     {
         /// <summary>
         /// Required designer variable.
@@ -28,23 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReporteEstadoDocentes));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReporteEstadoAlumnos));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.lblTitulo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbCursosReporte = new System.Windows.Forms.ComboBox();
+            this.dgvEstadoAlumnos = new System.Windows.Forms.DataGridView();
             this.chartReporte = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.dgvEstadoDocentes = new System.Windows.Forms.DataGridView();
-            this.labelTitulo = new System.Windows.Forms.Label();
             this.buttonCerrrar = new System.Windows.Forms.Button();
             this.buttonExportar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEstadoAlumnos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartReporte)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvEstadoDocentes)).BeginInit();
             this.SuspendLayout();
             // 
             // panel2
@@ -55,13 +55,53 @@
             resources.ApplyResources(this.panel2, "panel2");
             this.panel2.Name = "panel2";
             // 
-            // lblTitulo
+            // label1
             // 
-            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
-            this.lblTitulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.lblTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            resources.ApplyResources(this.lblTitulo, "lblTitulo");
-            this.lblTitulo.Name = "lblTitulo";
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Name = "label1";
+            // 
+            // cbCursosReporte
+            // 
+            resources.ApplyResources(this.cbCursosReporte, "cbCursosReporte");
+            this.cbCursosReporte.FormattingEnabled = true;
+            this.cbCursosReporte.Name = "cbCursosReporte";
+            this.cbCursosReporte.SelectedIndexChanged += new System.EventHandler(this.cbCursosReporte_SelectedIndexChanged);
+            // 
+            // dgvEstadoAlumnos
+            // 
+            this.dgvEstadoAlumnos.AllowUserToAddRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.dgvEstadoAlumnos.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            resources.ApplyResources(this.dgvEstadoAlumnos, "dgvEstadoAlumnos");
+            this.dgvEstadoAlumnos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvEstadoAlumnos.BackgroundColor = System.Drawing.Color.White;
+            this.dgvEstadoAlumnos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvEstadoAlumnos.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgvEstadoAlumnos.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(172)))), ((int)(((byte)(13)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvEstadoAlumnos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvEstadoAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(172)))), ((int)(((byte)(13)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvEstadoAlumnos.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvEstadoAlumnos.EnableHeadersVisualStyles = false;
+            this.dgvEstadoAlumnos.Name = "dgvEstadoAlumnos";
+            this.dgvEstadoAlumnos.RowHeadersVisible = false;
+            this.dgvEstadoAlumnos.RowTemplate.Height = 24;
             // 
             // chartReporte
             // 
@@ -89,47 +129,6 @@
             title1.Text = "Reporte";
             this.chartReporte.Titles.Add(title1);
             // 
-            // dgvEstadoDocentes
-            // 
-            this.dgvEstadoDocentes.AllowUserToAddRows = false;
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
-            this.dgvEstadoDocentes.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            resources.ApplyResources(this.dgvEstadoDocentes, "dgvEstadoDocentes");
-            this.dgvEstadoDocentes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
-            this.dgvEstadoDocentes.BackgroundColor = System.Drawing.Color.White;
-            this.dgvEstadoDocentes.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.dgvEstadoDocentes.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
-            this.dgvEstadoDocentes.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(172)))), ((int)(((byte)(13)))));
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvEstadoDocentes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
-            this.dgvEstadoDocentes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(172)))), ((int)(((byte)(13)))));
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvEstadoDocentes.DefaultCellStyle = dataGridViewCellStyle3;
-            this.dgvEstadoDocentes.EnableHeadersVisualStyles = false;
-            this.dgvEstadoDocentes.Name = "dgvEstadoDocentes";
-            this.dgvEstadoDocentes.RowHeadersVisible = false;
-            this.dgvEstadoDocentes.RowTemplate.Height = 24;
-            // 
-            // labelTitulo
-            // 
-            resources.ApplyResources(this.labelTitulo, "labelTitulo");
-            this.labelTitulo.BackColor = System.Drawing.Color.Transparent;
-            this.labelTitulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.labelTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.labelTitulo.Name = "labelTitulo";
-            // 
             // buttonCerrrar
             // 
             resources.ApplyResources(this.buttonCerrrar, "buttonCerrrar");
@@ -152,21 +151,22 @@
             this.buttonExportar.UseVisualStyleBackColor = false;
             this.buttonExportar.Click += new System.EventHandler(this.buttonExportar_Click);
             // 
-            // frmReporteEstadoDocentes
+            // frmReporteEstadoAlumnos
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.buttonCerrrar);
             this.Controls.Add(this.buttonExportar);
-            this.Controls.Add(this.labelTitulo);
-            this.Controls.Add(this.dgvEstadoDocentes);
             this.Controls.Add(this.chartReporte);
+            this.Controls.Add(this.dgvEstadoAlumnos);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.lblTitulo);
-            this.Name = "frmReporteEstadoDocentes";
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbCursosReporte);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmReporteEstadoAlumnos";
             this.Load += new System.EventHandler(this.frmReporteEstadoAlumno_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEstadoAlumnos)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartReporte)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvEstadoDocentes)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -174,10 +174,10 @@
 
         #endregion
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbCursosReporte;
+        private System.Windows.Forms.DataGridView dgvEstadoAlumnos;
         private System.Windows.Forms.DataVisualization.Charting.Chart chartReporte;
-        private System.Windows.Forms.DataGridView dgvEstadoDocentes;
-        private System.Windows.Forms.Label labelTitulo;
         private System.Windows.Forms.Button buttonCerrrar;
         private System.Windows.Forms.Button buttonExportar;
     }
---
+++ AppGestion/CapaPresentacion/frmReporteEstadoAlumnos.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaNegocio;
+
+namespace CapaPresentacion
+{
+    public partial class frmReporteEstadoAlumnos : Form
+    {
+        readonly N_ReporteEstadoAlumnos oReporteEstado = new N_ReporteEstadoAlumnos();
+        readonly N_CursosDocente oCursosDocente = new N_CursosDocente();
+        readonly N_Docente oDocente = new N_Docente();
+        private string CodDocente;
+        public frmReporteEstadoAlumnos(string pCodDocente)
+        {
+            InitializeComponent();
+            CodDocente = pCodDocente;
+        }
+
+        private void MostrarItemsComboBox(string[] Asignaturas)
+        {
+            //Mostrar cursos
+            cbCursosReporte.Items.AddRange(Asignaturas);
+        }
+        
+        private void MostrarReporte(string IdCatalogo)
+        {
+            dgvEstadoAlumnos.DataSource = oReporteEstado.MostrarReporteEstado(IdCatalogo, DateTime.Now);
+        }
+
+        private void frmReporteEstadoAlumno_Load(object sender, EventArgs e)
+        {
+            
+            //Obtener cursos que dicta el docente
+            string[] Asignaturas = oDocente.CursosDocente(CodDocente);
+            if (Asignaturas != null) //Si tiene asignaturas dictando
+            {
+                MostrarItemsComboBox(Asignaturas); //Mostrar opciones en comboBox
+                cbCursosReporte.SelectedIndex = 0;
+
+                //Obtener codCursoAsignatura
+                string codCursoAsig = cbCursosReporte.Text.Substring(0, 6);
+                string codCatalogo = oCursosDocente.ObtenerCodCatalogo(codCursoAsig);
+                MostrarReporte(codCatalogo); //Mostrar reporte de plan de sesiones
+
+                //Mostrar datos en el PieChart
+                MostrarPieChart();
+            }
+
+        }
+
+        private void MostrarPieChart()
+        {
+            DataTable tabla = dgvEstadoAlumnos.DataSource as DataTable;
+            Dictionary<string, float> dictEstados = new Dictionary<string, float>();
+            string estado;
+            foreach (DataRow row in tabla.Rows)
+            {
+                estado = row[2].ToString();
+                if(dictEstados.ContainsKey(estado))
+                    dictEstados[estado]++;
+                else
+                    dictEstados[estado] = 1;
+            }
+
+            // Obteniendo la suma de todos los valores
+            var total = dictEstados.Skip(0).Sum(v => v.Value);
+
+            foreach (string key in dictEstados.Keys)
+            {
+                chartReporte.Series["Estado"].Points.AddXY(key, dictEstados[key]/total);
+            }
+        }
+
+        public void ExportarDatos(DataGridView listadoCatalogo)
+        {
+            Microsoft.Office.Interop.Excel.Application exportarCatalogo = new Microsoft.Office.Interop.Excel.Application();
+            exportarCatalogo.Application.Workbooks.Add(true);
+            int indexColumn = 0;
+            foreach (DataGridViewColumn columna in listadoCatalogo.Columns)
+            {
+                if (columna.Name != "EDITAR")
+                {
+                    indexColumn++;
+                    exportarCatalogo.Cells[1, indexColumn] = columna.Name;
+                }
+            }
+            int indexfila = 0;
+            foreach (DataGridViewRow fila in listadoCatalogo.Rows)
+            {
+                indexfila++;
+                indexColumn = 0;
+                foreach (DataGridViewColumn columna in listadoCatalogo.Columns)
+                {
+                    if (columna.Name != "EDITAR")
+                    {
+                        indexColumn++;
+                        exportarCatalogo.Cells[indexfila + 1, indexColumn] = fila.Cells[columna.Name].Value;
+                    }
+                }
+            }
+            exportarCatalogo.Visible = true;
+        }
+
+
+
+        private void cbCursosReporte_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Obtener codCursoAsignatura
+            string codCursoAsig = cbCursosReporte.Text.Substring(0, 6);
+            string codCatalogo = oCursosDocente.ObtenerCodCatalogo(codCursoAsig);
+            //Actualizar reporte con los datos de la asignatura selecionada
+            MostrarReporte(codCatalogo);
+        }
+
+        private void buttonExportar_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("¿Seguro que desea exportar?", "Alerta", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)//Cerrar ventana
+                ExportarDatos(dgvEstadoAlumnos);
+        }
+
+        private void buttonCerrrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
